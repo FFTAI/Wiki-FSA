9f39080143e1286abefd1f6c028c05b7e9c9c188
Jason Chen <chenxin199305@163.com>
2023-08-21 15:42:55 +0800

    Version : V 1.2.27
    Update :
    1. 优化代码结构。

--- sdk-python/v3/demo_home_offset_set.py
+++ sdk-python/v3/demo_home_offset_set.py
@@ -11,13 +11,22 @@ def main():
 
     if server_ip_list:
 
-        # get the communication configuration of all FAS
         home_offset = numpy.ones(len(server_ip_list)) * 10
 
         for i in range(len(server_ip_list)):
             fi_fsa.set_home_offset(server_ip_list[i], home_offset[i])
 
         print('\n')
+
+        for i in range(len(server_ip_list)):
+            fi_fsa.get_home_offset(server_ip_list[i])
+
+        print('\n')
+
+        # Notice: home offset is not saved in flash, unless you obviously save it.
+        # for i in range(len(server_ip_list)):
+        #     fi_fse.save_config(server_ip_list[i])
+        # print('\n')
 
         # reboot all FAS
         for i in range(len(server_ip_list)):
