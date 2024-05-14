6a36edfe5d36b45858ea27eab8ea948732093404
whlphlgcn <705843711@qq.com>
2024-05-14 10:28:52 +0800

    Version : V 1.2.4
    1，修复demo_fast_set_enable.py循环使能的问题

--- sdk-python/v1/demo_fast_set_enable.py
+++ sdk-python/v1/demo_fast_set_enable.py
@@ -9,19 +9,11 @@ def main():
 
     if server_ip_list:
 
-        for j in range(10000):
-            # start_time = time.time()
+        for i in range(len(server_ip_list)):
+            _, = fi_fsa.fast_set_enable(server_ip_list[i])
 
-            for i in range(len(server_ip_list)):
-                _, = fi_fsa.fast_set_enable(server_ip_list[i])
 
-            # end_time = time.time()
-            #
-            # # print the latency of the pvc function
-            # latency = end_time - start_time
-            # print(latency * 1000, "ms")
-
-            time.sleep(0.2)  # sleep 1 second
+        time.sleep(0.2)  # sleep 1 second
 
 
 if __name__ == '__main__':
