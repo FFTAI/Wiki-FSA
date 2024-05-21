acb72191bfdbe7ba04a83291f5f6c95227956e5a
whlphlgcn <705843711@qq.com>
2024-05-21 11:10:38 +0800

    Version : V 1.2.8
    1，修改udp log，新增时间戳功能。

--- sdk-python/v2/udp_logger.py
+++ sdk-python/v2/udp_logger.py
@@ -4,19 +4,19 @@ import threading
 
 
 udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
+t = time.time()
 
 def udp_log_enable():
     remote_addr = ('192.168.137.51', 6666)
     while True:
         udp_socket.sendto('Hello, UDP!'.encode(), remote_addr)
-        time.sleep(0.01)
+        time.sleep(1)
 
 
 def udp_log_recv():
     while True:
         data, addr = udp_socket.recvfrom(2048)
-        print(f'Received data from {addr}: {data.decode()}')
+        print(str(int(round((time.time() - t) * 1000))) + f' Received data from {addr}: {data.decode()}')
 
 
 def main():
