0bc7298013cd386ebd5878cad38628a2edacb479
wcfalding <wcfalding@users.noreply.github.com>
2024-07-18 13:31:15 +0800

    获取firmware以及driver版本
    获取firmware以及driver版本

--- v2/example/demo_comm_driver_firmware_version_get.py
+++ v2/example/demo_comm_driver_firmware_version_get.py
@@ -11,9 +11,14 @@ def main():
 
         # get the communication configuration of all FAS
         for i in range(len(server_ip_list)):
-            fi_fsa_v2.get_comm_driver_version(server_ip_list[i])
+            driver_version = fi_fsa_v2.get_comm_driver_version(server_ip_list[i])
+            
+            firmware_version = fi_fsa_v2.get_comm_firmware_version(server_ip_list[i])
 
-        print("\n")
+            print("{{\"driver_version\":\"{}\"}}".format(driver_version))
+
+            print("{{\"firmware_version\":\"{}\"}}".format(firmware_version)
+)
         time.sleep(1)
 
 
