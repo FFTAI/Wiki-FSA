3aba7fc36093b3d75bcb8a083d608370dac63d78
whlphlgcn <705843711@qq.com>
2024-11-16 17:54:12 +0800

    [FEATURE][EMB-149][V2执行器esp32，python脚本fast接口pd电流前馈功能] [分析] [当前fast接口pd控制无电流前馈] [方案] [增加fast接口pd控制无电流前馈] [影响] [无] [测试] [通过python脚本测试带电流前馈的pd位置，结果符合预期] [自测] [通过python脚本测试带电流前馈的pd位置，结果符合预期]

--- v2/example/demo_fast_set_pd_position.py
+++ v2/example/demo_fast_set_pd_position.py
@@ -10,7 +10,7 @@ def main():
     if server_ip_list:
 
         for i in range(len(server_ip_list)):
-            fi_fsa_v2.fast_set_pd_control(server_ip_list[i], 0.0, 0.0)
+            fi_fsa_v2.fast_set_pd_control(server_ip_list[i], 0.0, 0.0, 0.0)
 
         time.sleep(1)  # sleep 1 second
 
