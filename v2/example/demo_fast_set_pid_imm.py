4b95d5d8564ec3837392bacb3afc2dcaa6047340
whlphlgcn <705843711@qq.com>
2024-11-22 15:00:40 +0800

    [BUG][EMB-163][FSA V2 Python脚本fast接口pid设置变量名不合适的问题] [分析] [fast_set_pid_imm参数名不合适] [方案] [修改fast_set_pid_imm参数名] [影响] [无] [测试] [无] [自测] [无]

--- v2/example/demo_fast_set_pid_imm.py
+++ v2/example/demo_fast_set_pid_imm.py
@@ -8,12 +8,11 @@ def main():
     server_ip_list = fi_fsa_v2.broadcast_func_with_filter(filter_type="Actuator")
 
     if server_ip_list:
-
-        kp = 0
-        ki = 0
-        kd = 0
+        position_kp = 0
+        velocity_kp = 0
+        velocity_ki = 0
         for i in range(len(server_ip_list)):
-            _ = fi_fsa_v2.fast_set_pid_imm(server_ip_list[i], kp, ki, kd)
+            _ = fi_fsa_v2.fast_set_pid_imm(server_ip_list[i], position_kp, velocity_kp, velocity_ki)
 
         time.sleep(0.2)  # sleep 1 second
 
