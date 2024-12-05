24ad30631c31a0366bfe469a036af922bd070c62
whlphlgcn <705843711@qq.com>
2024-12-05 20:20:26 +0800

    [FEATURE][EMB-222][检测版本匹配(V2执行器 0.3.9.9版本匹配)] [分析] [无] [方案] [将匹配fsa v2 esp32 v0.3.9.9版本的demo加入版本校验功能] [影响] [无] [测试] [运行demo脚本测试，结果符合预期] [自测] [运行demo脚本测试，结果符合预期]

--- v2/example/demo_ctrl_config_get.py
+++ v2/example/demo_ctrl_config_get.py
@@ -9,6 +9,14 @@ def main():
 
     if server_ip_list:
 
+        match_version = "0.3.9.9"
+        for i in range(len(server_ip_list)):
+            firmware_version = fi_fsa_v2.get_comm_firmware_version(server_ip_list[i])
+            if fi_fsa_v2.version_compare(firmware_version, match_version) < 0:
+                print("the firmware version of %s is %s, less than the match version %s" % (
+                    server_ip_list[i], firmware_version, match_version))
+                return
+
         # get the communication configuration of all FAS
         for i in range(len(server_ip_list)):
             fi_fsa_v2.get_config(server_ip_list[i])
--- v2/example/demo_fast_control_position_mode.py
+++ v2/example/demo_fast_control_position_mode.py
@@ -10,6 +10,14 @@ def main():
 
     if server_ip_list:
 
+        match_version = "0.3.9.9"
+        for i in range(len(server_ip_list)):
+            firmware_version = fi_fsa_v2.get_comm_firmware_version(server_ip_list[i])
+            if fi_fsa_v2.version_compare(firmware_version, match_version) < 0:
+                print("the firmware version of %s is %s, less than the match version %s" % (
+                    server_ip_list[i], firmware_version, match_version))
+                return
+
         fsa_state = True
         for i in range(len(server_ip_list)):
             fsa_state = fi_fsa_v2.get_state(server_ip_list[i])
