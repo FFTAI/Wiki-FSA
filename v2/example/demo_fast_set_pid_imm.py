d5b138424f100b479c704de12b6310a75648c728
whlphlgcn <705843711@qq.com>
2024-12-05 20:42:03 +0800

    [FEATURE][EMB-222][检测版本匹配(V2执行器 0.3.12.1版本匹配)] [分析] [无] [方案] [将匹配fsa v2 esp32 v0.3.12.1版本的demo加入版本校验功能] [影响] [无] [测试] [运行demo脚本测试，结果符合预期] [自测] [运行demo脚本测试，结果符合预期]

--- v2/example/demo_fast_set_pid_imm.py
+++ v2/example/demo_fast_set_pid_imm.py
@@ -8,6 +8,15 @@ def main():
     server_ip_list = fi_fsa_v2.broadcast_func_with_filter(filter_type="Actuator")
 
     if server_ip_list:
+
+        match_version = "0.3.12.1"
+        for i in range(len(server_ip_list)):
+            firmware_version = fi_fsa_v2.get_comm_firmware_version(server_ip_list[i])
+            if fi_fsa_v2.version_compare(firmware_version, match_version) < 0:
+                print("the firmware version of %s is %s, less than the match version %s" % (
+                    server_ip_list[i], firmware_version, match_version))
+                return
+
         position_kp = 0
         velocity_kp = 0
         velocity_ki = 0
